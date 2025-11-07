ac37cfea5285a158089109a0dfe98d3b8b26c636
lx_08 <balanskiboris1991@gmail.com>
2025-11-07 10:59:54 +0700

    feat: add 🛑 comments to maximumWealth example

--- array/1672_richest_customer_wealth.py
+++ array/1672_richest_customer_wealth.py
@@ -1,6 +1,6 @@
 
 from typing import List #🛑(automatic)
-accounts = [[2,8,7],[7,1,3],[1,9,5]]
+accounts = [[2,8,7],[7,1,3],[1,9,5]]#🛑
 
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
